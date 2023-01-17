5f8b613a949e1c376e57fce4d11b4c2daf4bd2d1
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-01-17 21:11:11 +0100

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,28 @@
+from microbit import *
+
+while True:
+    display.show(Image.CLOCK1)
+    sleep(1000)
+    display.show(Image.CLOCK2)
+    sleep(1000)
+    display.show(Image.CLOCK3)
+    sleep(1000)
+    display.show(Image.CLOCK4)
+    sleep(1000)
+    display.show(Image.CLOCK5)
+    sleep(1000)
+    display.show(Image.CLOCK6)
+    sleep(1000)
+    display.show(Image.CLOCK7)
+    sleep(1000)
+    display.show(Image.CLOCK8)
+    sleep(1000)
+    display.show(Image.CLOCK9)
+    sleep(1000)
+    display.show(Image.CLOCK10)
+    sleep(1000)
+    display.show(Image.CLOCK11)
+    sleep(1000)
+    display.show(Image.CLOCK12)
+    sleep(1000)
+
