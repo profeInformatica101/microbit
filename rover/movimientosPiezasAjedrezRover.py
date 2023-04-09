afa63ba0486d31eb1b2353e7b5e9eaba7f727c7e
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-04-09 20:17:21 +0200

    Update movimientosPiezasAjedrezRover.py

--- rover/movimientosPiezasAjedrezRover.py
+++ rover/movimientosPiezasAjedrezRover.py
@@ -7,6 +7,7 @@ rover = Micro_Rover()
 
 # Code in a 'while True:' loop repeats forever
 while True:
+   #Realizar movimientos de caballo
    if button_a.is_pressed():
        display.scroll("Caballo")
        rover.moverCelda(1)
@@ -14,6 +15,7 @@ while True:
        rover.moverCelda(1)
        rover.girarIzquierda()
        rover.moverCelda(1)
+   #Realizar movimientos de Torre   
    elif button_b.is_pressed():
        display.scroll("Torre")
        rover.moverCelda(8)
